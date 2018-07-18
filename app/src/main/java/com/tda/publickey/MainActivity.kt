16cdae47b2589fc5cadc6b8a9a56817864c73a48
anouar hammami <med.anouar.hammami@gmail.com>
2018-07-18 12:18:53 +0100

    Initial commit

---
+++ app/src/main/java/com/tda/publickey/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.tda.publickey
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import org.spongycastle.asn1.*
+import org.spongycastle.asn1.x509.SubjectPublicKeyInfo
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var kp: KeyPair? = null
+        try {
+            val kpg = KeyPairGenerator.getInstance("RSA")
+            kpg.initialize(512)
+
+            kp = kpg.generateKeyPair()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        val pubBytes = kp!!.public.encoded
+
+        val spkInfo = SubjectPublicKeyInfo.getInstance(pubBytes)
+        val primitive = spkInfo.publicKeyData.encoded
+txt.text=DERUTF8String.fromByteArray(primitive).toString()
+    //    Log.d("xxxx", DERUTF8String.fromByteArray(primitive).toString())
+    }
+}
